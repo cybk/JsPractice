89a2397bbc89b7e437c585f5c7818d095024e5b3
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2020-04-22 21:47:25 -0500

    some small practice with methods and updated the tabs on eslint

--- 02_Functions/functions.js
+++ 02_Functions/functions.js
@@ -1,3 +1,11 @@
 function calculateBill() {
-        // This is function body
+    // This is function body
+    console.log('Calculate bill is running');
+    const total = 100 * 1.13;
+
+    return Math.round(total);
 }
+
+const total = calculateBill();
+
+console.log(`Total: $${total}`);
