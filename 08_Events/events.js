cd7e73f0c8f6186d7d2c428e1ccd99fe984b2c90
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2020-05-04 22:14:35 -0500

    some other minot changes on events

--- 08_Events/events.js
+++ 08_Events/events.js
@@ -13,7 +13,20 @@ coolButts.addEventListener('click', hooray);
 const buyButons = Array.from(document.querySelectorAll('.buy'));
 
 const applyEvent = item => {
-    item.addEventListener('click', () => console.log('Buying Something'));
+    item.addEventListener('click', event => {
+        console.log(parseFloat(event.target.dataset.price));
+        event.stopPropagation();
+    });
 };
 
 buyButons.forEach(applyEvent);
+
+const photoEl = document.querySelector('.photo');
+photoEl.addEventListener('mousemove', e => {
+    console.log(e.currentTarget);
+});
+//--------------------------------------------------------------------------------
+window.addEventListener('click', event => {
+    console.log('You Clicked on the window');
+    console.log(event.target);
+});
